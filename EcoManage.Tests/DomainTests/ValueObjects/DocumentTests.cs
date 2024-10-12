233a1ea95b22f78b921016552498ccaf73b23854
Vitor Galache Jaconely <vitorgalache5@gmail.com>
2024-10-12 15:11:37 -0300

    Refatorando testes de documentos

--- EcoManage.Tests/DomainTests/ValueObjects/DocumentTests.cs
+++ EcoManage.Tests/DomainTests/ValueObjects/DocumentTests.cs
@@ -7,31 +7,55 @@ namespace EcoManage.Tests.DomainTests.ValueObjects;
 public class DocumentTests
 {
     [TestMethod]
-    public void Deve_retornar_erro_ao_inserir_cpf_vazio_()
+    [DataRow("  ",true)]
+    [DataRow("",true)]
+    [DataRow("           ",true)]
+    public void Deve_retornar_erro_ao_inserir_cpf_vazio_(string number,bool expectedResult)
     {
-        var document = new Document("",EDocumentType.Cpf);
-        Assert.AreEqual(document.Invalid,true);
+        var cpf = new Document(number,EDocumentType.Cpf);
+
+        var result = cpf.Invalid;
+        
+        Assert.AreEqual(expectedResult,result);
     }
 
     [TestMethod]
-    public void Deve_retornar_erro_ao_inserir_cnpj_vazio()
+    [DataRow("  ",true)]
+    [DataRow("",true)]
+    [DataRow("           ",true)]
+    public void Deve_retornar_erro_ao_inserir_cnpj_vazio(string number, bool expectedResult)
     {
-        var document = new Document("", EDocumentType.Cnpj);
-        Assert.AreEqual(document.Invalid,true);
+        var cnpj = new Document(number, EDocumentType.Cnpj);
+
+        var result = cnpj.Invalid;
+        
+        Assert.AreEqual(expectedResult,result);
     }
 
     [TestMethod]
-    public void Deve_retornar_sucesso_ao_inserir_cpf_com_11_caracteres()
+    [DataRow("12345678910",true)]
+    [DataRow("11987654321",true)]
+    
+    public void Deve_retornar_sucesso_ao_inserir_cpf_com_11_caracteres(string number,bool expectedResult)
     {
-        var document = new Document("12345678910",EDocumentType.Cpf);
-        Assert.AreEqual(document.Valid,true);
+        var cpf = new Document(number,EDocumentType.Cpf);
+
+        var result = cpf.Valid;
+        
+        Assert.AreEqual(expectedResult,result);
     }
 
     [TestMethod]
-    public void Deve_retornar_sucesso_ao_inserir_cnpj_com_14_caracteres()
+    [DataRow("29398282000109",true)]
+    [DataRow("12345678912345",true)]
+    [DataRow("23023982000132",true)]
+    public void Deve_retornar_sucesso_ao_inserir_cnpj_com_14_caracteres(string number,bool expectedResult)
     {
-        var document = new Document("12345678912345",EDocumentType.Cnpj);
-        Assert.AreEqual(document.Valid,true);
+        var cnpj = new Document(number,EDocumentType.Cnpj);
+
+        var result = cnpj.Valid;
+        
+        Assert.AreEqual(expectedResult,result);
     }
     
 }
